c62547e61170c32e101cee2b56f028f209aa0584
koiolk <pbochenek05@gmail.com>
2025-12-05 09:56:53 +0100

    Add files via upload
    dodatkowe zmiany

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+
 def zaloguj_sie():
     messagebox.showinfo("Udalo sie!", "Pomyslnie zalogowano!")
 
@@ -11,33 +12,47 @@ def pokaz_haslo():
     entry_haslo.config(show="")
     pokaz_haslo_button.config(text="Nie pokazuj", command=nie_pokazuj_hasla)
 
+def klik():
+    print("Tutaj zmieni na rejestracje")
+
 okno = Tk()
 
 okno.title("Księgarnia")
-
+okno.geometry('430x300')
+ 
 tekst_zaloguj = Label(okno, text="Zaloguj")
-tekst_zaloguj.grid(padx=5, pady=5)
+tekst_zaloguj.grid(row=0,padx=5, pady=5)
+
 tekst_login = Label(okno, text="Login: ")
-tekst_login.grid(padx=5, pady=5)
+tekst_login.grid(row=1,padx=5, pady=5)
 
 entry_login = Entry(okno)
-entry_login.grid(padx=5, pady=5)
+entry_login.grid(row=2,padx=5, pady=5)
 
 
 tekst_haslo = Label(okno, text="Hasło: ")
-tekst_haslo.grid(padx=5, pady=5)
+tekst_haslo.grid(row=3,padx=5, pady=5)
 
 entry_haslo = Entry(okno, show="*")
-entry_haslo.grid(padx=5, pady=5)
+entry_haslo.grid(row=4,column=0,padx=5, pady=5)
+
 
 pokaz_haslo_button = Button(okno, text="Pokaz", command=pokaz_haslo)
-pokaz_haslo_button.grid(padx=5, pady=5) 
+pokaz_haslo_button.grid(row=5,padx=15, pady=5) 
 
-testowy_label = Label(okno)
+
 
 zaloguj = Button(okno, text="Zaloguj", command=zaloguj_sie)
-tekst_rejestracja = Label(okno, text=f"Jesli nie masz konta to -> Stwórz Konto")
+tekst_rejestracja = Label(okno, text=f"Jesli nie masz konta to")
+rejestracja = Label(okno, text=f"Stwórz Konto", fg='blue', cursor="hand2")
 
-zaloguj.grid(padx=5, pady=5),tekst_rejestracja.grid(padx=5, pady=5)
+zaloguj.grid(row=6,padx=5, pady=5)
+
+tekst_rejestracja.grid(row=7)
+rejestracja.grid(row=8)
+rejestracja.bind("<Button-1>",lambda event:klik())
+
+okno.grid_columnconfigure(0,weight=1)
+
 
 okno.mainloop()
